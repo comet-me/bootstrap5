54b758cee96342013227c55583ab3f3b94859f06
sigenaba <niceone.jologs@gmail.com>
2021-06-27 21:22:15 +0800

    popover.html added

--- js/myscript.js
+++ js/myscript.js
@@ -132,3 +132,9 @@
         document.execCommand('copy');
         window.getSelection().removeAllRanges();
       }
+
+      // Popover c'mon baby
+      var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+        var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+        })
